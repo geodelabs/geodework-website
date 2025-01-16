65b1e0ea93a0358849cf0b01cad0b2013e8cf0d4
Paul Wackerow <54227730+wackerow@users.noreply.github.com>
2025-01-16 14:11:55 -0800

    feat: add skeleton subscribe form

--- components/Subscribe.tsx
+++ components/Subscribe.tsx
@@ -3,7 +3,28 @@ const Subscribe = () => (
     <div className="text-center font-mono">
       <strong>subscribe</strong> to our newsletter
     </div>
-    {/* TODO: Add input and button */}
+    <form className="flex items-center gap-2 max-sm:flex-col">
+      <input
+        type="email"
+        placeholder="email"
+        className="w-full bg-primary-light px-4 py-1 text-body placeholder:text-body/80"
+        style={{
+          clipPath:
+            "polygon(0 0, 100% 0, 100% 100%, 0.5rem 100%, 0 calc(100% - 0.375rem)",
+        }}
+      />
+      <button
+        // TODO: Make and use ui/button
+        type="submit"
+        className="bg-accent px-4 py-1 text-primary text-white hover:bg-accent-dark"
+        style={{
+          clipPath:
+            "polygon(0 0, calc(100% - 0.5rem) 0, 100% 0.375rem, 100% 100%, 0 100%)",
+        }}
+      >
+        subscribe
+      </button>
+    </form>
   </div>
 )
 
