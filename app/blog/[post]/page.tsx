1292c31bc1535caae076e4652e93ff5b26b6f43c
Paul Wackerow <54227730+wackerow@users.noreply.github.com>
2025-01-16 15:23:20 -0800

    feat: add navigation to neighboring posts

--- app/blog/[post]/page.tsx
+++ app/blog/[post]/page.tsx
@@ -5,7 +5,7 @@ import matter from "gray-matter"
 
 import GeodeIcon from "@/components/GeodeIcon"
 import { MarkdownProvider } from "@/components/Markdown/Provider"
-import Subscribe from "@/components/Subscribe"
+import Link from "@/components/ui/link"
 
 import { dateToUTCNoonDate, matchFilename } from "@/lib/blog"
 import { BLOG_CONTENT_DIR } from "@/lib/constants"
@@ -26,11 +26,24 @@ export default async function BlogPost({
     throw new Error(`Missing blog content directory: ${BLOG_CONTENT_DIR}`)
 
   // Expected filename is in format YYYY-MM-DD-some-title.md; used to match URL
-  if (!fs.existsSync(filepath)) throw new Error(`Missing blog post: ${post}`)
+  if (!fs.existsSync(filepath)) notFound()
 
   const match = matchFilename(file)
 
   if (!match) notFound()
+
+  const allPosts = fs
+    .readdirSync(BLOG_CONTENT_DIR)
+    .filter((f) => matchFilename(f))
+    .sort((a, b) => {
+      const dateA = dateToUTCNoonDate(matchFilename(a)![1])
+      const dateB = dateToUTCNoonDate(matchFilename(b)![1])
+      return dateB.getTime() - dateA.getTime()
+    })
+  const currentIndex = allPosts.findIndex((f) => f === file)
+  const lastIndex = allPosts.length - 1
+  const prevIndex = currentIndex === 0 ? null : currentIndex - 1
+  const nextIndex = currentIndex === lastIndex ? null : currentIndex + 1
 
   const markdownContent = fs.readFileSync(filepath, "utf8")
   const { data, content } = matter(markdownContent)
@@ -64,6 +77,33 @@ export default async function BlogPost({
 
         <GeodeIcon className="mx-auto my-8" />
       </article>
+
+      <nav className="flex gap-2 text-center">
+        {prevIndex !== null && (
+          <Link
+            href={`/blog/${allPosts[prevIndex].replace(/\.md$/, "")}`}
+            style={{
+              clipPath:
+                "polygon(0 0, 100% 0, 100% 100%, 0.5rem 100%, 0 calc(100% - 0.375rem)",
+            }}
+            className="w-[16ch] bg-accent py-2 text-primary hover:bg-accent-dark"
+          >
+            previous
+          </Link>
+        )}
+        {nextIndex !== null && (
+          <Link
+            href={`/blog/${allPosts[nextIndex].replace(/\.md$/, "")}`}
+            style={{
+              clipPath:
+                "polygon(0 0, calc(100% - 0.5rem) 0, 100% 0.375rem, 100% 100%, 0 100%)",
+            }}
+            className="w-[16ch] bg-accent py-2 text-primary hover:bg-accent-dark"
+          >
+            next
+          </Link>
+        )}
+      </nav>
     </div>
   )
 }
