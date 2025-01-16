6b4b14df93aa179328d03e3171edf8eaaaf80237
Paul Wackerow <54227730+wackerow@users.noreply.github.com>
2025-01-16 14:12:21 -0800

    feat: create GeodeIcon component

---
+++ components/GeodeIcon.tsx
@@ -0,0 +1,11 @@
+import { cn } from "@/lib/utils"
+import GeodeGlyph from "@/public/images/geode-glyph.svg"
+
+const GeodeIcon = ({
+  className,
+  ...props
+}: React.SVGAttributes<SVGElement>) => (
+  <GeodeGlyph className={cn("text-[2rem]", className)} {...props} />
+)
+
+export default GeodeIcon
