a1d2a61b09bf4f0d427edaa510a0c8328d0d6d20
toshiboy86 <toshibowie86@gmail.com>
2025-04-01 14:00:40 +0900

    feat: add function to shorten Ethereum addresses

---
+++ lib/web3.ts
@@ -0,0 +1,7 @@
+export function shortenAddress(address: string): string {
+    if (address.startsWith("0x")) {
+      return `${address.slice(0, 6)}...${address.slice(-6)}`
+    }
+    return `${address.slice(0, 8)}...${address.slice(-8)}`
+  }
+  
