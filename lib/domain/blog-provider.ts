9bcf5dd164ac9f26bdd727fdacfbc3027b392ea8
toshiboy86 <toshibowie86@gmail.com>
2025-04-09 13:09:11 +0900

    feat: implement BlogProvider for Local Ethereum blog integration

---
+++ lib/domain/blog-provider.ts
@@ -0,0 +1,42 @@
+import { LOCAL_ETHEREUM_BLOG_URL, EBlogProvider } from "../constants"
+
+export class BlogProvider {
+  constructor(
+    private readonly slug: string,
+    private readonly blogProvider: string
+  ) {
+    this.slug = slug
+    this.blogProvider = blogProvider
+  }
+
+  // NOTE: Make it another class when more blog providers are added
+  getMethodsByBlogProvider(blogProvider: string) {
+    switch (blogProvider) {
+      case EBlogProvider.LOCAL_ETHEREUM:
+        return {
+          getBlogPostUrl: () => `${LOCAL_ETHEREUM_BLOG_URL}/p/${this.slug}`,
+          getIsBlogPostExists: async (url: string) => {
+            const response = await fetch(url, { method: "HEAD" })
+            return !new URL(response.url).searchParams.get("404")
+          },
+        }
+      default:
+        return {
+          getBlogPostUrl: () => "",
+          getIsBlogPostExists: async () => false,
+        }
+    }
+  }
+
+  getBlogPostUrl() {
+    const methods = this.getMethodsByBlogProvider(this.blogProvider)
+    return methods.getBlogPostUrl()
+  }
+
+  async getIsBlogPostExists(): Promise<boolean> {
+    const methods = this.getMethodsByBlogProvider(this.blogProvider)
+    const url = methods.getBlogPostUrl()
+    console.log("...url", url)
+    return await methods.getIsBlogPostExists(url)
+  }
+}
