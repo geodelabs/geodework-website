990a92527077185e1aebcae3ba8259fa749f31df
johnshift <johnshift.dev@gmail.com>
2025-10-02 18:53:25 +0800

    feat(fourthwall): add product details fetch fn

--- lib/fourthwall/api.ts
+++ lib/fourthwall/api.ts
@@ -1,19 +1,20 @@
-import { storefrontProductsResponseSchema } from "@/lib/schemas/fourthwall"
+import {
+  productSchema,
+  storefrontProductsResponseSchema,
+} from "@/lib/schemas/fourthwall"
 
 const API_URL = "https://storefront-api.fourthwall.com/v1"
 const API_TOKEN = process.env.NEXT_PUBLIC_FOURTHWALL_STOREFRONT_TOKEN!
-const BATCH_SIZE = 9
+const BATCH_SIZE = 6
 const COLLECTION_SLUG = "geode-merche"
-
-interface Props {
-  page: number
-  batchSize?: number
-}
 
 export const fetchProductListPage = async ({
   page,
   batchSize = BATCH_SIZE,
-}: Props) => {
+}: {
+  page: number
+  batchSize?: number
+}) => {
   const query = new URLSearchParams({
     page: page.toString(),
     size: batchSize.toString(),
@@ -26,6 +27,29 @@ export const fetchProductListPage = async ({
 
   const json = await res.json()
   const parseResult = storefrontProductsResponseSchema.safeParse(json)
+  if (!parseResult.success) throw new Error("Invalid response from API")
+
+  return parseResult.data
+}
+
+export const fetchProductDetails = async ({
+  slug,
+  currency = "USD",
+}: {
+  slug: string
+  currency?: string
+}) => {
+  const query = new URLSearchParams({
+    currency,
+    storefront_token: API_TOKEN,
+  })
+  const url = `${API_URL}/products/${slug}?${query.toString()}`
+
+  const res = await fetch(url, { cache: "no-store" })
+  if (!res.ok) throw new Error("Failed to fetch product details")
+
+  const json = await res.json()
+  const parseResult = productSchema.safeParse(json)
   if (!parseResult.success) throw new Error("Invalid response from API")
 
   return parseResult.data
